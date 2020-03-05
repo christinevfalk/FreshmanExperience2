f06243e7b25621bb0807dabcd3b1d8dac2f4df5d
oliviaerickson <47121792+oliviaerickson@users.noreply.github.com>
2020-03-05 08:40:22 -0600

    Created data and schema for DB

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions (
+    id          INT             NOT NULL AUTO_INCREMENT,
+    title       VARCHAR(255)    NOT NULL,
+    instructor  VARCHAR(255)    NOT NULL,
+    instructor  VARCHAR(255)    NOT NULL,
+    PRIMARY KEY (id)
+    );
