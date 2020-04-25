d9394915e465d11d3603cd8f4eac0febb6fff529
oliviaerickson <47121792+oliviaerickson@users.noreply.github.com>
2020-04-25 14:15:50 -0500

    Edit student and styling

--- src/main/java/edu/ucmo/FreshmanExperience/Controller/UserController.java
+++ src/main/java/edu/ucmo/FreshmanExperience/Controller/UserController.java
@@ -1,21 +1,14 @@
 package edu.ucmo.FreshmanExperience.Controller;
 
 import edu.ucmo.FreshmanExperience.Dao.UserDao;
-import edu.ucmo.FreshmanExperience.Model.Sessions;
 import edu.ucmo.FreshmanExperience.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import edu.ucmo.FreshmanExperience.Service.MyUserDetailsService;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.*;
-
-
 
 
 
@@ -34,19 +27,4 @@ public class UserController {
         userDao.findAll().iterator().forEachRemaining(list::add);
         return list;
     }
-
-    @RequestMapping(value = "/edit/{ucmoid}")
-    public ModelAndView showEditUserPage(@PathVariable(name= "ucmoid") String ucmoid) {
-        ModelAndView mav = new ModelAndView("edit_user");
-        User user = service.get(ucmoid);
-        mav.addObject("user", user);
-
-        return mav;
-
-    }
-
-
-
-
-
 }
